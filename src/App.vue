7aa9c5c39bf02a2db5587a2b4e52fde538192a0e
= <luccashiroshi@gmail.com>
2022-06-10 15:08:32 -0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
-<template>
+ <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Problema de Montyhall</h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount">Quantas portas?</label>
+        <input
+          type="text"
+          id="portsAmount"
+          size="3"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort">Qual porta é premiada?</label>
+        <input
+          type="text"
+          id="selectedPort"
+          size="3"
+          v-model.number="selectedPort"
+        />
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+ 
+ <script>
+import Door from "./components/WbDoor.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Door },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null,
+    };
+  },
+};
 </script>
+ 
+ <style>
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
 
-<style>
+body {
+  color: #fff;
+  background: linear-gradient(to right, rgb(19, 78, 94), rgb(19, 100, 94));
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  font-size: 3rem;
+  margin-bottom: 60px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form,
+.form input,
+.form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+.form input {
+  margin-left: 10px;
+  border: 1px solid #999;
+  border-radius: 5px;
+}
+
+.form button {
+  padding: 8px;
+  border: 2px solid #999;
+  border-radius: 5px;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+
+  flex-wrap: wrap;
 }
 </style>
